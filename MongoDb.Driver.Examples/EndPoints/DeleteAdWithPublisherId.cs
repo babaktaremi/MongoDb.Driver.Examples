1affa82e64ae5ec024d05d67f2c74607f9854161
Babak Taremi <babaktaremi@yahoo.com>
2023-11-18 20:00:24 +0330

    Added Delete Example

---
+++ MongoDb.Driver.Examples/EndPoints/DeleteAdWithPublisherId.cs
@@ -0,0 +1,32 @@
+﻿using Carter;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDb.Driver.Examples.Models.Ads;
+
+namespace MongoDb.Driver.Examples.EndPoints;
+
+public class DeleteAdWithPublisherId:ICarterModule
+{
+    private readonly IMongoDatabase _mongoDatabase;
+
+    public DeleteAdWithPublisherId(IMongoDatabase mongoDatabase)
+    {
+        _mongoDatabase = mongoDatabase;
+    }
+
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/DeleteAd", async (string adPublisherId, string adId) =>
+        {
+            if (!ObjectId.TryParse(adPublisherId, out var adPublisherObjectId) ||
+                !ObjectId.TryParse(adId, out var adObjectId))
+                return Results.BadRequest();
+
+            var adsCollection = _mongoDatabase.GetCollection<AdModel>(AdModel.CollectionName);
+
+            var deleteResult=await adsCollection.DeleteOneAsync(c => c.AdPublisher == adPublisherObjectId && c.AdId == adObjectId);
+
+            return Results.NoContent();
+        });
+    }
+}
